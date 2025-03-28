cc0c7d1cc415d1f2c463d8ddefbf035a5f0ed553
sun Jiao <jiaosun@abeam.com>
2025-03-28 05:03:31 +0000

    test2

--- myui5app/webapp/controller/ProductDetail.controller.js
+++ myui5app/webapp/controller/ProductDetail.controller.js
@@ -1,32 +1,49 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageTost"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageTost) {
         "use strict";
 
         return Controller.extend("myui5app.controller.ProductDetail", {
             onInit: function () {
-            const oRouter=sap.ui.core.UIComponent.getRouterFor(this);
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 
-            oRouter.getRoute("RouteProductDetail").attachMatched(this._attachMatched,this);            
+                oRouter.getRoute("RouteProductDetail").attachMatched(this._attachMatched, this);
             },
 
-            _attachMatched:function(oEvent){
-                const iProductId= oEvent.getParameter("arguments").productId;
+            _attachMatched: function (oEvent) {
+                const iProductId = oEvent.getParameter("arguments").productId;
 
-               const oView=this.getView();
+                const oView = this.getView();
 
-               oView.bindElement({
-                path:"/Products("+ iProductId +")",
-                events:{
-                    dataRequest:function(){  oView.setBusy(true);},
-                    dataRecived:function(){ oView.setBusy(false);}
+                oView.bindElement({
+                    path: "/Products(" + iProductId + ")",
+                    events: {
+                        dataRequest: function () { oView.setBusy(true); },
+                        dataRecived: function () { oView.setBusy(false); }
+                    }
+
+                });
+            },
+            addToCart: function (oEvent) {
+                MessageTost.show('add to cart');
+            },
+            markAsFav: function (oEvent) {
+                const oButton = oEvent.getSource();
+                if (oButton.getICon() === "sap-icon://unfavorite") {
+                    oButton.setIcon("sap-icon://favorite");
+                    MessageTost.show("Added to favorite");
+                    return;
                 }
 
-               });
+                oButton.setIcon("sap-icon://unfavorite");
+                MessageTost.show("remove from favorite")
+
+
             }
         });
     });
